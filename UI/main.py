ca56bc8b6acc412ce52efda4a936c3393e65ceb2
JakobThuesen <jakobt@live.dk>
2025-04-23 12:07:30 +0200

    edited from cpp to python because i forgot

---
+++ UI/main.py
@@ -0,0 +1,11 @@
+import sys
+
+def complex(x,y):
+    x = x
+
+    if 0:
+        print(x , y)
+
+
+
+complex(0,2j)
--- UI/ui_control.py
+++ UI/ui_control.py
@@ -4,12 +4,25 @@ char ui_control::key_input()
 {
     char key;
     std::cin >> key;
+
     
+
     return key;
 }
 
 void ui_control::mode_selection(int mode)
 {
+
+    switch(mode){
+        case 1:
+        std::cout << "Hej med jer";
+        break;
+        case 2:
+        std::cout << "Hej med jer 2";
+        break;
+        default:
+        std::cout << "Der er ingen indstillinger med følgende nummer, indtast venligst et nyt" << std::endl;
+    }
 }
 
 void ui_control::show_data(int indoor_light_level, int outdoor_light_level, bool person_detected, bool lamp_on, bool shades_down, int lamp_level)
