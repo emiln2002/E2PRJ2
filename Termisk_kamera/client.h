67159f1f820f75103311205fc4a07a5952d462d6
kfredholm <kasp939i@gmail.com>
2025-05-07 12:30:07 +0200

    vol3

---
+++ Termisk_kamera/client.h
@@ -0,0 +1,24 @@
+
+#pragma once
+#include <iostream>
+#include <WiFi.h>
+using namespace std;
+
+class client
+{
+private:
+    const int port;
+    const char * host_ip;
+    const char* ssid;
+
+    String data_send;
+    
+public:
+    client(const int port = 8080, const char * host_ip = "172.20.10.2", const char* ssid = "smart");
+
+    void send(String data);
+
+    String read();
+    
+};
+
